625b93643bd22a1061cc5e62286bb805b5c7c00e
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-02-06 15:02:16 +0100

    Update Ticket.java

--- src/cinema/model/Ticket.java
+++ src/cinema/model/Ticket.java
@@ -6,15 +6,20 @@ import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import java.util.UUID;
 
 public class Ticket {
+    
     @JsonUnwrapped
     private String token;
+    
     @JsonProperty(value = "ticket")
     private Seat seat;
+    
     public Ticket(){}
+    
     public Ticket(Seat seat) {
         this.token = UUID.randomUUID().toString();
         this.seat = seat;
     }
+    
     public String getToken() {
         return token;
     }
