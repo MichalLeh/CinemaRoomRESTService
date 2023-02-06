1e2b2f3a26c6a1ff48855152e1db47950a3380c9
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-02-06 14:35:39 +0100

    Update ScreenRoom.java

--- src/cinema/model/ScreenRoom.java
+++ src/cinema/model/ScreenRoom.java
@@ -9,13 +9,14 @@ import java.util.List;
 
 public class ScreenRoom {
 	@JsonProperty("total_rows")
-    private int totalRows;
-    @JsonProperty("total_columns")
+    	private int totalRows;
+    	@JsonProperty("total_columns")
 	private int totalColumns;
 	@JsonProperty("available_seats")
 	private List<Seat> availableSeats = new ArrayList<>();
 	@JsonIgnore
     private HashMap<String, Seat> purchasedSeats = new HashMap<>();
+	
 	public ScreenRoom(int totalRows, int totalColumns) {
 		this.totalRows = totalRows;
 		this.totalColumns = totalColumns;
