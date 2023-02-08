4dd18286c3b2ef0ecca7b7250017335b8e770955
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-02-08 12:30:44 +0100

    Update SeatController.java
    Add comments

--- src/cinema/presentation/SeatController.java
+++ src/cinema/presentation/SeatController.java
@@ -15,14 +15,15 @@ import java.util.Map;
 public class SeatController {
 	private final ScreenRoom screenRoom = new ScreenRoom(9, 9);
 	private Stats stats = new Stats(screenRoom.getTotalRows(), screenRoom.getTotalColumns());
-	
 	public SeatController() {}
-	
+
+	// Returns information about the rows, columns, and available seats as a JSON object.
 	@GetMapping("/seats")
 	public ScreenRoom getSeat(){
 		return screenRoom;
 	}
-
+	// Receives a seat as a JSON object and returns a JSON object with a token, row, column and price fields.
+    // If the seat is taken or user pass a wrong row/column number, respond with a 400 (Bad Request) status code.
 	@PostMapping("/purchase")
 	public synchronized ResponseEntity<?> postSeat(@RequestBody Seat seat) {
 		if ((seat.getRow() >= 1 && seat.getRow() <= 9) && (seat.getColumn() >= 1 && seat.getColumn() <= 9)) {
@@ -42,7 +43,8 @@ public class SeatController {
 			return new ResponseEntity<>(Map.of("error", "The number of a row or a column is out of bounds!"), HttpStatus.BAD_REQUEST);
 		}
 	}
-
+	// Receives a ticket as a JSON object and returns a JSON object with a row, column and price fields. Also makes the seat available again.
+	// If the ticket cannot be identified by the token, respond with a 400 status code.
 	@PostMapping("/return")
 	public synchronized ResponseEntity<?> postReturn(@RequestBody Ticket ticket){
 		if(screenRoom.getPurchasedSeats().containsKey(ticket.getToken())){
@@ -57,7 +59,8 @@ public class SeatController {
 		}
 		return new ResponseEntity<>(Map.of("error", "Wrong token!"), HttpStatus.BAD_REQUEST);
 	}
-
+	// Returns the movie theatre if the URL parameters contain a password key with a super_secret value.
+	// If the parameters don't contain a password key or a wrong value has been passed, respond with a 401 status code.
 	@PostMapping("/stats")
 	public ResponseEntity<?> postStats(@RequestParam(required = false) String password){
 		if (password == null || !password.equals("super_secret")){
